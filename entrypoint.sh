e49c2c33e47ff05149e1af5ae9a47e4ba57c500e
chwps <as2pik390.gaming@gmail.com>
2025-08-12 13:29:45 +0200

    fix: Crash when launching

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@
 CRON_SCHEDULE=${CRON_SCHEDULE:-0 */1 * * *}
 
 # Write cron line and launch crond
-echo "$CRON_SCHEDULE /usr/local/bin/python /app/app.py" | crontab -
+echo "$CRON_SCHEDULE /usr/local/bin/python /app/app.py >> /var/log/sync.log 2>&1" | crontab -
 exec crond -f
